e89519cac5bd738edb37620ccd7dff529a7edc83
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-11 20:16:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,14 +1,47 @@
-def using_push(array, element)
-  colors_in_the_rainbow =["red", "orange", "yellow", "green", "blue", "indigo"]
-  colors_in_the_rainbow.push("violet")
+def using_push(array, string)
+  array.push(string)
 end
 
-def using_unshift(array, element)
-  bouroughs_in_nyc =["Bronx", "Brooklyn", "Manhattan", "Queens"]
-  bouroughs_in_nyc.unshift("Staten Island")
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
-def using_pop
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, item)
+  array.insert(4, item)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, num)
+  array.delete_at(num)
 end

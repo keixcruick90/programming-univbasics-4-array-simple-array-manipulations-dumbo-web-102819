05a5bdc55a88c0924223422b490494b7c8a2ff12
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-11 20:14:02 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,14 @@
+def using_push(array, element)
+  colors_in_the_rainbow =["red", "orange", "yellow", "green", "blue", "indigo"]
+  colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(array, element)
+  bouroughs_in_nyc =["Bronx", "Brooklyn", "Manhattan", "Queens"]
+  bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+end
